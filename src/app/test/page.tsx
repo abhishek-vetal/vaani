1e195124a561c191e9f25c730548f164458d3922
Abhishek Vetal <abhivetal8825@gmail.com>
2026-06-20 13:20:21 +0530

    04 backend infrastructure

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,21 @@
+import { HydrateClient, prefetch, trpc } from '@/trpc/server';
+import { ClientGreeting } from './client-greeting';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
 export default function TestPage() {
-  return <div>This is test page.</div>;
+  prefetch(trpc.healthCheck.queryOptions());
+
+  return (
+    <HydrateClient>
+      <div className='flex flex-col items-center justify-center font-bold text-3xl p-7 gap-4'>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <Suspense fallback={<div>Loading...</div>}>
+            tRPC Test Page
+            <ClientGreeting />
+          </Suspense>
+        </ErrorBoundary>
+      </div>
+    </HydrateClient>
+  );
 }
